9236248d9b1facb2d076b05996e962c8ddd95930
ajit1519 <34963359+ajit1519@users.noreply.github.com>
2019-10-24 12:28:36 +0530

    Update Program.cs

--- gitpractice/gitpractice/Program.cs
+++ gitpractice/gitpractice/Program.cs
@@ -11,6 +11,7 @@ namespace gitpractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello C#");
+            Console.WriteLine("Hello .Net");
         }
     }
 }
